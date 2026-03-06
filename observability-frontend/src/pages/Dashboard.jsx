98f7addbc17d9e29cbb4df20a5eeaccc77f8603d
Nour-triaa <nourchenetriaa2002@gmail.com>
2026-03-07 00:07:13 +0100

    Ajout ingress Grafana + mise à jour Dashboard

--- observability-frontend/src/pages/Dashboard.jsx
+++ observability-frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,49 @@
-import { Typography } from "@mui/material";
+import { Typography, Box, Card, CardContent } from "@mui/material";
+
 export default function Dashboard() {
-  return <Typography variant="h4">Dashboard Page</Typography>;
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        backgroundColor: "#111827", // fond principal
+        padding: 4,
+      }}
+    >
+      {/* Titre */}
+      <Typography
+        variant="h3"
+        sx={{
+          fontWeight: "bold",
+          marginBottom: 4,
+          color: "#ffffff", // texte blanc
+        }}
+      >
+        Dashboard
+      </Typography>
+
+      {/* Carte Grafana */}
+      <Card
+        sx={{
+          width: "80%",
+          maxWidth: 900,
+          borderRadius: 3,
+          boxShadow: 6,
+          backgroundColor: "#1f2937", // fond de la carte
+        }}
+      >
+        <CardContent>
+          <iframe
+            src="http://grafana.local/d-solo/adlwcvp/chrome?orgId=1&panelId=1"
+            width="100%"
+            height="400"
+            style={{ border: "none", backgroundColor: "#1f2937" }}
+          />
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
